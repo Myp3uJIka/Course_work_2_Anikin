3d1486b335e39707c6ceec628010f6300b694b2b
Мефодий <storage@list.ru>
2022-01-09 08:18:50 +1000

    Спец. задание 1: добавление представления для тэгов; создание функций для обработки тэгов и добавления тэгов в JSON публикаций, корректировка tag.html.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
-from functions import read_json, search_comments, get_post, search_content_post, search_user_post, comments_in_posts
+from functions import read_json, search_comments, get_post, search_content_post, search_user_post, comments_in_posts, \
+    search_tags, get_post_with_tag
 
 POSTS_PATH = 'data/posts.json'
 COMMENTS_PATH = 'data/comments.json'
@@ -9,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')  # декоратор для стартовой страницы
 def start_page():
-    comments_in_posts(POSTS_PATH, COMMENTS_PATH)
+    comments_in_posts(POSTS_PATH, COMMENTS_PATH)  # обновление информации о количестве постов
     posts = read_json(POSTS_PATH)
     return render_template('index.html', posts=posts)
 
@@ -18,12 +19,11 @@ def start_page():
 def get_comments(post_id):
     comments = search_comments(COMMENTS_PATH, int(post_id))  # получения списка комментариев к посту
     post = get_post(POSTS_PATH, int(post_id))  # получение данных указанной публикации
-    len_comments = len(comments)  # вычесление количества комментариев для шаблона
-    return render_template('post.html', post=post, comments=comments, len_comments=len_comments)
+    return render_template('post.html', post=post, comments=comments, post_id=post_id)
 
 
 @app.route('/search/')  # вывод постов по совпадению с описанием
-def find_posts():
+def find_posts():  # TODO сделать переходи из формы
     s = request.args.get('s')
     posts = search_content_post(POSTS_PATH, s)  # получение списка совпадений
     posts_count = len(posts)  # получение количества сопадений
@@ -34,6 +34,13 @@ def find_posts():
 def find_user_posts(username):
     posts = search_user_post(POSTS_PATH, username)  # получение списка постов
     return render_template('user-feed.html', posts=posts)
+
+
+@app.route('/tag/<tagname>')
+def find_tag(tagname):
+    search_tags(POSTS_PATH)  # обновление информации о количестве тегов в публикациях
+    posts = get_post_with_tag(POSTS_PATH, tagname)  # получение списка постов по совпадению с тегом
+    return render_template('tag.html', posts=posts, tag_name=tagname)
 
 
 if __name__ == '__main__':
--- functions.py
+++ functions.py
@@ -7,32 +7,31 @@ def read_json(filename):  # функция для чтения json-файла
     return data
 
 
-def comments_in_posts(posts, comments):
+def comments_in_posts(posts, comments):  # функция для добавления количества комментариев к посту
     posts_list = read_json(posts)
     comments_list = read_json(comments)
-    comments_count_list = {}
-    for comment in comments_list:
-        coincidence = False
+    comments_count_list = {}  # словарь пост/количество комментариев
+    for comment in comments_list:  # перебор комментариев
+        coincidence = False  # переменная для указания совпадения комментария и публикации
         for key in comments_count_list.keys():
             if comment['post_id'] == key:
-                comments_count_list[key] += 1
+                comments_count_list[key] += 1  # увеличение значения количества комментариев на 1 при наличии
+                # совпадения в списке комментариев
                 coincidence = True
         if not coincidence:
-            comments_count_list[comment['post_id']] = 1
-    for post in posts_list:
+            comments_count_list[comment['post_id']] = 1  # создание новой записи в словаре списка комментариев при
+            # несовпадении комментария и существующих записей в словаре
+    for post in posts_list:  # добавление новой записи (количество комментариев) в список словарей с публикациями
         coincidence = False
         for comment, count in comments_count_list.items():
             if post['pk'] == comment:
                 post['comments_count'] = count
                 coincidence = True
         if not coincidence:
-            post['comments_count'] = 0
-    with open(posts, 'w', encoding='utf-8') as f:
+            post['comments_count'] = 0  # нулевое значение при отсутствии комментариев к публикации
+    with open(posts, 'w', encoding='utf-8') as f:  # запись в файл
         json.dump(posts_list, f, sort_keys=False, indent=4, ensure_ascii=False, separators=(',', ': '))
     return
-
-
-
 
 
 def search_comments(comments, post_id):  # функция для поиска комментариев к посту по номеру публикации
@@ -71,3 +70,28 @@ def search_user_post(posts, user_name):  # функция для поиска п
             result.append(post)
     return result
 
+
+def search_tags(posts):  # функция для извлечения списка тегов и добавления их к посту
+    data = read_json(posts)
+    for post in data:
+        tags_list = set()  # создание множества (без повторения одинаковых значений)
+        words = post['content'].split()  # создание словаря из слов в описании поста для дальнейшей обработки
+        for teg in words:
+            if teg.startswith('#'):
+                tags_list.add(teg[1:])  # добавление в множество совпадений по словам начинающимся с '#'
+        tags = list(tags_list)  # создание списка для дальнейшей сортировки по алфавиту
+        tags.sort()  # сортировка списка
+        post['tags'] = tags  # создание записи к посту со всеми тегами
+    with open(posts, 'w', encoding='utf-8') as f:
+        json.dump(data, f, sort_keys=False, indent=4, ensure_ascii=False, separators=(',', ': '))
+    return
+
+
+def get_post_with_tag(posts, tag_name):  # получение списка постов по совпадению с тегом
+    data = read_json(posts)
+    result = []
+    for post in data:
+        if post['tags']:  # проверка: пустой ли список тэгов
+            if tag_name in post['tags']:
+                result.append(post)  # добавление постов с совпадением в финальный список
+    return result
